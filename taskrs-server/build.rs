6ccef9624820629459c664ffd4a95a508422c0fc
Stefan Rupertsberger <s.rupertsberger@gmail.com>
2021-09-18 20:25:05 +0200

    Create permission constants by build script from json files

--- taskrs-server/build.rs
+++ taskrs-server/build.rs
@@ -4,7 +4,7 @@ use std::fs::{File, OpenOptions};
 use std::io::{BufReader, Write};
 
 fn main() {
-    let permissions: String = glob("permissions/**/*.json").unwrap()
+    let permissions: Vec<Permission> = glob("permissions/**/*.json").unwrap()
         .into_iter()
         .filter_map(|e| e.ok())
         .map(|path| {
@@ -14,8 +14,13 @@ fn main() {
             serde_json::from_reader::<_, Vec<Permission>>(reader).unwrap()
         })
         .flatten()
+        .collect();
+
+    // SQL migrations
+    let migration_content: String = permissions
+        .iter()
         .map(|p| {
-            match p.description {
+            match &p.description {
                 None => {
                     format!("INSERT INTO permissions (name, \"group\") VALUES ('{}', '{}');", p.name, p.group)
                 }
@@ -27,15 +32,31 @@ fn main() {
         .collect::<Vec<String>>()
         .join("\n");
 
-
+    // Create migration directory
     std::fs::create_dir_all("migrations/99999999999999_permissions").unwrap();
     let mut up_file = OpenOptions::new().write(true).truncate(true).create(true).open("migrations/99999999999999_permissions/up.sql").unwrap();
     let mut down_file = OpenOptions::new().write(true).truncate(true).create(true).open("migrations/99999999999999_permissions/down.sql").unwrap();
     let down_statement = String::from("DELETE FROM permissions;");
 
     // Write INSERT statements
-    up_file.write_all(permissions.as_bytes()).unwrap();
+    up_file.write_all(migration_content.as_bytes()).unwrap();
     down_file.write_all(down_statement.as_bytes()).unwrap();
+
+    // Permission constants
+    let constants: String = permissions
+        .iter()
+        .map(|p| {
+            format!(
+                "pub const {}: &str = \"{}\";",
+                p.name.to_uppercase(),
+                p.name
+            );
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    let mut permissions_file = OpenOptions::new().write(true).truncate(true).create(true).open("src/permissions.rs").unwrap();
+    permissions_file.write_all(constants.as_bytes()).unwrap();
 }
 
 #[derive(Debug, Default, Clone, Deserialize)]
